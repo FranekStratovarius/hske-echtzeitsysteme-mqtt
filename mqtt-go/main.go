0de9f3e937aecd2db1fd099f6b5911410276f0ef
FranekStratovarius <louishuss2000@gmail.com>
2024-04-25 09:17:05 +0200

    added esp32 and go code

---
+++ mqtt-go/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func checkTrafficLightImage(client MQTT.Client) {
+	out, err := exec.Command("python", "main.py").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("detected traffic light: %s", out)
+	switch string(out) {
+	case "green":
+		mqtt_publish(client, "drive", []byte("go"))
+	case "red":
+		mqtt_publish(client, "drive", []byte("stop"))
+	}
+}
+
+func mqtt_publish(client MQTT.Client, topic string, payload []byte) {
+	log.Println("Sample Publisher Started")
+	for i := 0; i < 1; i++ {
+		log.Println("---- doing publish ----")
+		token := client.Publish(topic, byte(0), false, payload)
+		token.Wait()
+	}
+}
+
+func mqtt_topic_image(client MQTT.Client, payload []byte) {
+	//log.Printf("%s", string(payload))
+	err := os.WriteFile("./light/img.jpg", payload, 0644)
+	check(err)
+	checkTrafficLightImage(client)
+}
+
+func mqtt_topic_default(topic string, payload string) {
+	log.Printf("[%s] %s", topic, payload)
+}
+
+func main() {
+	// myApp := app.New()
+	// w := myApp.NewWindow("Image")
+	// image := canvas.NewImageFromResource(theme.FyneLogo())
+	// image.FillMode = canvas.ImageFillOriginal
+	// w.SetContent(image)
+
+	// w.ShowAndRun()
+
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker("tcp://localhost:1883")
+
+	choke := make(chan bool)
+
+	opts.SetDefaultPublishHandler(func(client MQTT.Client, msg MQTT.Message) {
+		switch msg.Topic() {
+		case "/image":
+			go mqtt_topic_image(client, msg.Payload())
+		default:
+			go mqtt_topic_default(msg.Topic(), string(msg.Payload()))
+		}
+		choke <- true
+	})
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := client.Subscribe("#", byte(0), nil); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+		os.Exit(1)
+	}
+
+	// publish
+	mqtt_publish(client, "test", []byte("testpayload von go"))
+
+	// subscribe
+	for {
+		<-choke
+	}
+}
---
+++ mqtt-test/mqtt-test.ino
@@ -0,0 +1,149 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "esp_camera.h"
+// select camera model
+#define CAMERA_MODEL_AI_THINKER
+#include "camera_pins.h"
+
+const char ssid[] = "Handy von Louis";
+const char pass[] = "einsbisacht";
+
+WiFiClient net;
+PubSubClient client(net);
+IPAddress server(192, 168, 93, 109);
+
+unsigned long lastMillis = 0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i=0;i<length;i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("esp32")) {
+      Serial.println("connected");
+      client.publish("/hello","bin da.");
+      client.subscribe("/hello");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+
+  Serial.println("\nconnected!");
+}
+
+void setup_camera() {
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    if(psramFound()){
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID) {
+    s->set_vflip(s, 1); // flip it back
+    s->set_brightness(s, 1); // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, pass);
+
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+  client.setBufferSize(10000);
+
+  setup_camera();
+}
+
+void loop() {
+  //delay(10);  // <- fixes some issues with WiFi stability
+  if (!client.connected()) {
+    reconnect();
+  }
+  //Serial.printf("Taking picture...\n");
+  camera_fb_t *pic = esp_camera_fb_get();
+
+  if (pic) {
+    // use pic->buf to access the image
+    //Serial.printf("Picture taken! Its size was: %zu bytes\n", pic->len);
+    bool success = client.publish("/image", pic->buf, pic->len, false);
+    //Serial.printf("picture send success: %i\n", success);
+    // free memory
+    esp_camera_fb_return(pic);
+
+    //vTaskDelay(1000 / portTICK_RATE_MS);
+  }
+
+  client.loop();
+}
